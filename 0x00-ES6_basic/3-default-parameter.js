6a55bd4e3f3c2de1ee34102ea6d5f52c9979ebe4
lawsonredeye <omoregbeeolawson@gmail.com>
2024-06-25 21:37:09 +0100

    (Fix): deleted all package.json, package-lock.json, node_modules and the .eslintr and recopied them back to the 0x00-ES6_basic

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
-export function getSumOfHoods(initialNumber, expansion1989=89, expansion2019=19) {
-    return initialNumber + expansion1989 + expansion2019;
+export function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
-
--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,8 +1,8 @@
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-      [`income-${getCurrentYear()}`] : income,
-      [`gdp-${getCurrentYear()}`] : gdp,
-      [`capita-${getCurrentYear()}`] : capita
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
   return budget;
