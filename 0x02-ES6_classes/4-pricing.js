c72ee7da2b372bf9e86c321a934751c1885b2b31
lawsonredeye <omoregbeeolawson@gmail.com>
2024-06-28 00:20:36 +0100

    function which inherit from a class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,29 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    super
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency} ()`
+  }
+}
