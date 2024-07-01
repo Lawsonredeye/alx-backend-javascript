da0c89c9a40c7b663231bdde2ed06d8eddb20d39
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-01 11:33:08 +0100

    class named EVCar that extends the Car class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,3 +1,12 @@
-import Car from './10-cars';
+import Car from './10-car';
 
-class EVCar extends Car
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
