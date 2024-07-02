11cff5ba9f5b0c3717486c54ac94aca2a6dd9a02
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-02 09:23:19 +0100

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const arr = [
+    { id: 1, firstName: 'Guillaume', location: 'San francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San francisco' },
+  ];
+  return arr;
+}
