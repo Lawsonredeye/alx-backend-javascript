0d60b426217a4ac0301792d7015f473fae2677a2
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-03 22:49:11 +0100

    (fix): removed the declare

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,12 @@ interface Teacher {
   location: string;
   contract: any;
 }
+
+const teacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  yearsOfExperience: 21,
+  location: 'Ibadan',
+  contract: undefined
+}
