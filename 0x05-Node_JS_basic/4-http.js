b28f3fc6519c0ededbc2977495257971bcfc8b86
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-20 21:01:27 +0100

    (mod): set headers to text/plain

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.statusCode = 200;
   res.end('Hello Holberton School!');
 });
