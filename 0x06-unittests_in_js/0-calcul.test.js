dc5082795b73fa5434a40fb68dbae8a27c2bc050
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-22 04:21:07 +0100

    modified the mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,23 +5,41 @@ describe('smoke test', function() {
 	it('checks equality', function() {
 		assert.equal(calculateNumber(1, 3), 4);
 		assert.equal(calculateNumber(NaN, 8), NaN);
-	})
-})
-
-describe('equal test', function() {
+	});
 	it('checks equality', function() {
 		assert.equal(calculateNumber(1, 3.7), 5);
-	})
-})
-
-describe('mocha round test', function() {
+	});
 	it('checks equality', function() {
 		assert.equal(calculateNumber(1.2, 3.7), 5);
-	})
-})
-
-describe('round up test', function() {
+	});
 	it('checks equality', function() {
 		assert.equal(calculateNumber(1.5, 3.7), 6);
-	})
-})
+	});
+	it('checks negative equality', function() {
+		assert.equal(calculateNumber(-1.5, -3.7), -5);
+	});
+});
+
+// describe('equal test', function() {
+// 	it('checks equality', function() {
+// 		assert.equal(calculateNumber(1, 3.7), 5);
+// 	})
+// })
+
+// describe('mocha round test', function() {
+// 	it('checks equality', function() {
+// 		assert.equal(calculateNumber(1.2, 3.7), 5);
+// 	})
+// })
+
+// describe('round up test', function() {
+// 	it('checks equality', function() {
+// 		assert.equal(calculateNumber(1.5, 3.7), 6);
+// 	})
+// })
+
+// describe('negative testing', function() {
+// 	it('checks negative equality', function() {
+// 		assert.equal(calculateNumber(-1.5, -3.7), -5);
+// 	})
+// })
