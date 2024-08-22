3bc32a277c123bbc708fe24ec78185e21b41baf2
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-22 20:26:36 +0100

    testing with chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+// import { expect } from 'chai';
+// import { calculateNumber } from './2-calcul_chai.js';
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai.js');
+
+// expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+// expect(calculateNumber('SUM', 1.4, 1.4)).to.equal(2);
+
+
+describe('Test for mathematical operations', function() {
+  it('#sums', function() {
+    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+  });
+  it('Sums float nums', function() {
+    expect(calculateNumber('SUM', 1.4, 1.4).to.equal(2));
+  });
+  it('SUMs Negative nums', function() {
+    expect(calculateNumber('SUM', -3, -4).to.equal(-7));
+  });
+  it('Divides', function() {
+    expect(calculateNumber('DIVIDE',1.4, 4.5).to.equal(0.2));
+  });
+  it('Divides', function() {
+    expect(calculateNumber('DIVIDE', 1.4, 0).to.equal('Error'));
+  });
+  it('Subtract from denominator', function() {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5).to.equal(-4));
+  });
+})
